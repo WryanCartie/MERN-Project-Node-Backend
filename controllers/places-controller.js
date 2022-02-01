91e8091ed520e966596d221038ceea0243863c32
WryanCartie <33276404+WryanCartie@users.noreply.github.com>
2022-02-02 00:55:17 +0800

    Fixed a minor bug by moving the dummy places to controller and fixed the HTTPError import to the controller

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -1,5 +1,20 @@
 
 
+const HttpError = require('../models/http-error')
+
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Stockholm Royal Palace',
+        description: 'The Grand Palace of the Swedish Empire !!',
+        location : {
+            lat: 59.3268,
+            lng: 18.0717
+        },
+        address: 'Kungliga slottet, 107 70 Stockholm, Sweden',
+        creator: 'u1'
+    }
+]
 const getPlacesById = (req,res,next) =>{
     const placeId = req.params.pid;
     const place = DUMMY_PLACES.find(p=>{
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,22 +1,9 @@
 const express = require('express');
 
-const HttpError = require('../models/http-error')
+
 
 const placesController = require('../controllers/places-controller');
 
-const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Stockholm Royal Palace',
-        description: 'The Grand Palace of the Swedish Empire !!',
-        location : {
-            lat: 59.3268,
-            lng: 18.0717
-        },
-        address: 'Kungliga slottet, 107 70 Stockholm, Sweden',
-        creator: 'u1'
-    }
-]
 
 const router = express.Router();
 
