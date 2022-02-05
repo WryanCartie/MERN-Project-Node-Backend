93cf34e8c24ec0caec04907c48a8ec1161c9ed24
WryanCartie <33276404+WryanCartie@users.noreply.github.com>
2022-02-05 20:11:14 +0800

    Update location.js

---
+++ util/location.js
@@ -0,0 +1,11 @@
+function getCoordsForAddress(address) {
+    return (    
+        {
+            lat:40.7484474,
+            lng: -73.9871516
+        }
+    )
+    
+}
+
+export default getCoordsForAddress;
