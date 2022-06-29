5e695dce38a1d6cf055b6e93bd624a457afe5330
WryanCartie <33276404+WryanCartie@users.noreply.github.com>
2022-06-29 16:21:13 +0800

    Add user login functionality.

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -30,7 +30,7 @@ const signup = async (req,res,next) =>{
     try{
         existingUser = await User.findOne({email: email})
     }catch(err){
-        const error = new HttpError('Signup failed heree, please try again.',500)
+        const error = new HttpError('Signing up, please try again.',500)
         return next(error)
     }
 
@@ -57,12 +57,19 @@ const signup = async (req,res,next) =>{
     res.status(201).json({user: createdUser.toObject({getters: true}),message:'User Signup Sucessfull !!'})
 }
 
-const login = (req,res,next) =>{
+const login = async(req,res,next) =>{
  const {email,password} = req.body;
 
- const identifiedUser = DUMMY_USERS.find(u=> u.email = email);
- if(!identifiedUser && identifiedUser.password ===  password){
-     return next(new HttpError('Could not identify user, credentials seems to be wrong !!',401))
+ let existingUser;
+ try{ 
+     existingUser = await User.findOne({email: email})
+ }catch(err){
+     const error = new HttpError('Signing up, please try again.',500)
+     return next(error)
+ }
+ if(existingUser && existingUser.password !== password){
+    const error = new HttpError('Invalid Login, please try again.',401)
+    return next(error)
  }
     res.json({message:'Logged in sucessfull !!'})
 }
