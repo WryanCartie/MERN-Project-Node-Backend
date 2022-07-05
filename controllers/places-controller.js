44524956306ec40341bc7b15bc9f8ab4a15102ad
WryanCartie <33276404+WryanCartie@users.noreply.github.com>
2022-07-05 15:18:42 +0800

    Implement alternative method for getting place by user Id using populate.

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -45,9 +45,9 @@ const getPlacesById = async (req,res,next) =>{
 
 const getPlacesByUserId = async (req,res,next) =>{
     const userId = req.params.uid;
-    let places ;
+    let userWithPlaces;
     try{
-        places = await Place.find({creator:userId})
+        userWithPlaces = await User.findById(userId).populate('places');
     }catch(err){
         const error = new HttpError(
             'Something went wrong, could not find the place.',
@@ -55,13 +55,13 @@ const getPlacesByUserId = async (req,res,next) =>{
             );
         return next(error)
     }
-    if(!places || places.length == 0){
+    if(!userWithPlaces || userWithPlaces.length == 0){
         const error = new HttpError('Could not find places for the selected user id.',404)
     
         return next(error)
     }
 
-    res.json({places : places.map(place => place.toObject({getters:true})) })
+    res.json({places : userWithPlaces.places.map(place => place.toObject({getters:true})) })
 }
 
 const createPlace = async (req,res,next) =>{
