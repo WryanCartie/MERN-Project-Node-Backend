0b4d67321d8de9322940c1a97d35e53645634b6d
WryanCartie <33276404+WryanCartie@users.noreply.github.com>
2022-07-06 15:44:40 +0800

    Add middleware to deal with CORS error

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req,res,next)=>{
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,Authorization'
+  )
+  res.setHeader('Access-Control-Allow-Methods','GET,POST,PATCH,DELETE')
+  next();
+})
+
 app.use("/api/places", placesRoutes);
 
 app.use("/api/users", userRoutes);
@@ -31,7 +41,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    "mongodb+srv://Kruyoso:Anastasia15@cluster0.cx4p6z3.mongodb.net/places?retryWrites=true&w=majority"
+    "mongodb+srv://Kruyoso:Anastasia15@cluster0.cx4p6z3.mongodb.net/mern?retryWrites=true&w=majority"
   )
   .then((err) => {
     app.listen(5000);
