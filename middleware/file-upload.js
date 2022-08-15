6b7d107f5f303f585e7c04808823f8390ee64a10
Wryan Cartie Halim <33276404+WryanCartie@users.noreply.github.com>
2022-08-15 11:35:56 +0800

    Add fileFilter to add backend authenthication.

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -12,8 +12,13 @@ const fileUpload = multer({
             cb('null','uploads/images')
         },
         filename: (req,file,cb) =>{
-            const ext = MINE_TYPE_MAP(file.mimetype)
+            const ext = MINE_TYPE_MAP[file.mimetype]
             cb(nullm,uuid()+'.'+ext)
+        },
+        fileFilter: (req,file,cb) =>{
+            const isValid = !!MINE_TYPE_MAP[file.mintype]
+            const error = isValid ? null : new Error('Invalid mine type!!')
+            cb(error,isValid)
         }
     })
 })
