d87550480c74cb3ea27ba0ec74f3cd4aa53cb346
WryanCartie <wryancartie@gmail.com>
2022-09-17 16:19:43 +0800

    United the fronteend and backend to a full integrated app.

---
+++ public/precache-manifest.ca2ca1400f0384df63de886d306aafc5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e1f1fdbdb53f8dd93b202fc5b633d1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b40b1909a038ae63144",
+    "url": "/static/css/3.2476d1d2.chunk.css"
+  },
+  {
+    "revision": "611667831038e978238a",
+    "url": "/static/css/4.a7b25cca.chunk.css"
+  },
+  {
+    "revision": "872238a946d30e2cc783",
+    "url": "/static/css/5.860660ab.chunk.css"
+  },
+  {
+    "revision": "8de419ab72046e17f690",
+    "url": "/static/css/6.bf817a5c.chunk.css"
+  },
+  {
+    "revision": "0812263c02d5a4799a43",
+    "url": "/static/css/main.91e410f9.chunk.css"
+  },
+  {
+    "revision": "ea3f5ba3ac068f35ab26",
+    "url": "/static/js/2.49fc3281.chunk.js"
+  },
+  {
+    "revision": "9b40b1909a038ae63144",
+    "url": "/static/js/3.a6035e97.chunk.js"
+  },
+  {
+    "revision": "611667831038e978238a",
+    "url": "/static/js/4.18339d55.chunk.js"
+  },
+  {
+    "revision": "872238a946d30e2cc783",
+    "url": "/static/js/5.809a3f0a.chunk.js"
+  },
+  {
+    "revision": "8de419ab72046e17f690",
+    "url": "/static/js/6.d0081b36.chunk.js"
+  },
+  {
+    "revision": "0812263c02d5a4799a43",
+    "url": "/static/js/main.81d37d99.chunk.js"
+  },
+  {
+    "revision": "06448aa57722c0b23949",
+    "url": "/static/js/runtime-main.0dbd2050.js"
+  }
+]);
