c5a984c15c210303106c750d3510d8f1c1b00855
WryanCartie <wryancartie@gmail.com>
2022-09-17 16:55:05 +0800

    Fix minor typing error

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cx4p6z3.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then((err) => {
-    app.listen(process.env.port || 5000);
+    app.listen(process.env.PORT || 5000);
   })
   .catch((err) => {
     console.log(err);
