33766e65d78713a94451d7eba63ace0825dbcad1
WryanCartie <wryancartie@gmail.com>
2022-11-12 16:44:40 +0800

    Fix minor error in check-auth

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -7,13 +7,15 @@ module.exports = (req,res,next) =>{
         return next()
     }
     try{
+        
       
         const token= req.headers.authorization.split(' ')[1];
         if(!token){
             throw new Error('Authenthication failed');
         }
         const decodedToken = jwt.verify(token,process.env.JWT_KEY)
-        req.userData = {userId: decodedToken.id}
+       
+        req.userData = {userId: decodedToken.userId}
         next()
     }catch(err){
         console.log(err);
