1e5ca31647082cf726ae0a90328fa36bf344781e
WryanCartie <wryancartie@gmail.com>
2023-04-26 11:09:53 +0800

    Fix CORS error present in render.

--- app.js
+++ app.js
@@ -48,12 +48,8 @@ app.use(express.static(path.join('public')))
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
 
